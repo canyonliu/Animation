9bc7e60582def45d2067030a349f95358583330d
Albert_Liu <albert_liu@Albert-LiudeMac.local>
2015-01-22 21:01:36 +0800

    Initial Commit

---
+++ 动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  动画
+//
+//  Created by Albert_Liu on 15-1-22.
+//  Copyright (c) 2015年 Albert_Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 动画/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  动画
+//
+//  Created by Albert_Liu on 15-1-22.
+//  Copyright (c) 2015年 Albert_Liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
